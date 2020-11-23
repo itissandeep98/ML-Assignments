c03e3b1df0065d6005cfaa413920598b39fcafc4
sandeep <itissandeep98@gmail.com>
2020-11-23 22:21:38 +0530

    Assignment3 init

---
+++ Assignment3/Q1.py
@@ -0,0 +1,288 @@
+class MyNeuralNetwork():
+    """
+    My implementation of a Neural Network Classifier.
+    """
+
+    acti_fns = ['relu', 'sigmoid', 'linear', 'acti']
+    weight_inits = ['zero', 'random', 'normal']
+
+    def __init__(self, n_layers, layer_sizes, activation, learning_rate, weight_init, batch_size, num_epochs):
+        """
+        Initializing a new MyNeuralNetwork object
+
+        Parameters
+        ----------
+        n_layers : int value specifying the number of layers
+
+        layer_sizes : integer array of size n_layers specifying the number of nodes in each layer
+
+        activation : string specifying the activation function to be used
+                     possible inputs: relu, sigmoid, linear, tanh
+
+        learning_rate : float value specifying the learning rate to be used
+
+        weight_init : string specifying the weight initialization function to be used
+                      possible inputs: zero, random, normal
+
+        batch_size : int value specifying the batch size to be used
+
+        num_epochs : int value specifying the number of epochs to be used
+        """
+
+        if activation not in self.acti_fns:
+            raise Exception('Incorrect Activation Function')
+
+        if weight_init not in self.weight_inits:
+            raise Exception('Incorrect Weight Initialization Function')
+        pass
+
+    def relu(self, X):
+        """
+        Calculating the ReLU activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def relu_grad(self, X):
+        """
+        Calculating the gradient of ReLU activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def sigmoid(self, X):
+        """
+        Calculating the Sigmoid activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def sigmoid_grad(self, X):
+        """
+        Calculating the gradient of Sigmoid activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def linear(self, X):
+        """
+        Calculating the Linear activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def linear_grad(self, X):
+        """
+        Calculating the gradient of Linear activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def tanh(self, X):
+        """
+        Calculating the Tanh activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def tanh_grad(self, X):
+        """
+        Calculating the gradient of Tanh activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def softmax(self, X):
+        """
+        Calculating the ReLU activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def softmax_grad(self, X):
+        """
+        Calculating the gradient of Softmax activation for a particular layer
+
+        Parameters
+        ----------
+        X : 1-dimentional numpy array 
+
+        Returns
+        -------
+        x_calc : 1-dimensional numpy array after calculating the necessary function over X
+        """
+        return None
+
+    def zero_init(self, shape):
+        """
+        Calculating the initial weights after Zero Activation for a particular layer
+
+        Parameters
+        ----------
+        shape : tuple specifying the shape of the layer for which weights have to be generated 
+
+        Returns
+        -------
+        weight : 1-dimensional numpy array which contains the initial weights for the requested layer
+        """
+        return None
+
+    def random_init(self, shape):
+        """
+        Calculating the initial weights after Random Activation for a particular layer
+
+        Parameters
+        ----------
+        shape : tuple specifying the shape of the layer for which weights have to be generated 
+
+        Returns
+        -------
+        weight : 1-dimensional numpy array which contains the initial weights for the requested layer
+        """
+        return None
+
+    def normal_init(self, shape):
+        """
+        Calculating the initial weights after Normal(0,1) Activation for a particular layer
+
+        Parameters
+        ----------
+        shape : tuple specifying the shape of the layer for which weights have to be generated 
+
+        Returns
+        -------
+        weight : 1-dimensional numpy array which contains the initial weights for the requested layer
+        """
+        return None
+
+    def fit(self, X, y):
+        """
+        Fitting (training) the linear model.
+
+        Parameters
+        ----------
+        X : 2-dimensional numpy array of shape (n_samples, n_features) which acts as training data.
+
+        y : 1-dimensional numpy array of shape (n_samples,) which acts as training labels.
+        
+        Returns
+        -------
+        self : an instance of self
+        """
+
+        # fit function has to return an instance of itself or else it won't work with test.py
+        return self
+
+    def predict_proba(self, X):
+        """
+        Predicting probabilities using the trained linear model.
+
+        Parameters
+        ----------
+        X : 2-dimensional numpy array of shape (n_samples, n_features) which acts as testing data.
+
+        Returns
+        -------
+        y : 1-dimensional numpy array of shape (n_samples,) which contains the prediction probabilities.
+        """
+
+        # return the numpy array y which contains the predicted values
+        return None
+
+    def predict(self, X):
+        """
+        Predicting values using the trained linear model.
+
+        Parameters
+        ----------
+        X : 2-dimensional numpy array of shape (n_samples, n_features) which acts as testing data.
+
+        Returns
+        -------
+        y : 1-dimensional numpy array of shape (n_samples,) which contains the predicted values.
+        """
+
+        # return the numpy array y which contains the predicted values
+        return None
+
+    def score(self, X, y):
+        """
+        Predicting values using the trained linear model.
+
+        Parameters
+        ----------
+        X : 2-dimensional numpy array of shape (n_samples, n_features) which acts as testing data.
+
+        y : 1-dimensional numpy array of shape (n_samples,) which acts as testing labels.
+
+        Returns
+        -------
+        acc : float value specifying the accuracy of the model on the provided testing set
+        """
+
+        # return the numpy array y which contains the predicted values
+        return None
+
